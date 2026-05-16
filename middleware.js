8302a768190ea16f3729f6d527647dbb01696357
jerrypaul <jerrypaulsam@gmail.com>
2026-05-16 10:33:32 +0530

    fixed any errrors and bugs

--- middleware.js
+++ middleware.js
@@ -27,5 +27,7 @@ export function middleware(request) {
 }
 
 export const config = {
-  matcher: ['/((?!_next|favicon.ico).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
 }
